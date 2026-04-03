92c5d8f3c24bbfe43eb239d95d6a96201f2edf4a
Valentin Morello <78524203+Morello2004@users.noreply.github.com>
2026-04-03 15:31:45 -0300

    a ej1.c

--- TP1-Recursividad/punto 1/ej1.c
+++ TP1-Recursividad/punto 1/ej1.c
@@ -5,4 +5,21 @@ int main() {
 }
   
 bool palindromo(char *cad,int inicio,int fin){}
-//hacer el desarrollo recursivo
+	    int n = strlen(cadena);
+    if (n<=1){
+        return true;
+    }
+
+    if (cadena[0] != cadena[n-1]) {
+        return false;
+    }
+
+    char sub[100];
+
+    for (int i = 0; i < n-2;i++){
+        sub [i] =cadena[i+1];
+    }
+    sub[n-2]= '\0';
+
+    return palindromo(sub);
+}
